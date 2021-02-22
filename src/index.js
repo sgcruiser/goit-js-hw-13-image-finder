2b8531a86bea9ab3174598dd43d3b4d1868d25b3
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-02-22 22:42:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './scss/styles.scss';
-import debounce from 'lodash.debounce';
-import refs from './js/refs';
+import 'material-design-icons/iconfont/material-icons.css';
+
+import './js/page-search';
+import './js/api/apiService';
+import './js/search-images';
+import './js/image-modal';
